4c99ba6113174db16cfd61d2982a8e149bb5b78e
rdwaz7 <rdwaz7@gmail.com>
2021-02-21 09:20:22 +0200

    Commit our first model, model_1_V1.0

---
+++ Model_1/model_1.py
@@ -0,0 +1,60 @@
+import tensorflow.keras
+from PIL import Image, ImageOps
+import numpy as np
+
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
+
+# Load the model
+model = tensorflow.keras.models.load_model('keras_model.h5')
+
+# Create the array of the right shape to feed into the keras model
+# The 'length' or number of images you can put into the array is
+# determined by the first position in the shape tuple, in this case 1.
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+# Replace this with the path to your image
+image = Image.open('test_orange.jpg')
+
+#resize the image to a 224x224 with the same strategy as in TM2:
+#resizing the image to be at least 224x224 and then cropping from the center
+size = (224, 224)
+image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+#turn the image into a numpy array
+image_array = np.asarray(image)
+
+# display the resized image
+#climage.show()
+
+# Normalize the image
+normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+# Load the image into the array
+data[0] = normalized_image_array
+
+#Color labels used:
+labels=('yellow','blue','red','orange','black')
+
+# run the inference
+#print(type(prediction))
+
+# prediction is numpy.ndarray
+prediction = model.predict(data)
+
+print(prediction)
+#Convert the element in the prediction numpy.ndarray into a individual elements:
+color0=prediction[0][0]
+color1=prediction[0][1]
+color2=prediction[0][2]
+color3=prediction[0][3]
+
+#append the elements into a list:
+mylist=[color0,color1,color2,color3]
+
+#get the index of the max predection:
+label_max=mylist.index(max(mylist))
+print("The answer is : ")
+
+#print the predicted color 
+print(labels[label_max])
